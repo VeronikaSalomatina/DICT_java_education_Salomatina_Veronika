3b35f1b3e086672955be23b0d9cd8d606053b407
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 21:49:11 +0200

    Hangman {3-rd} stage

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,17 +1,35 @@
 import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class Hangman {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("HANGMAN");
-
-        String user = scanner.nextLine();
+        String user = input.nextLine();
         if (Objects.equals(user, "java")){
             System.out.println("You survived!");
         } else{
             System.out.println("You lost!");
+            ArrayList<String> lst = new ArrayList<>();
+            lst.add("python");
+            lst.add("java");
+            lst.add("javascript");
+            lst.add("kotlin");
+
+            String word = lst.get(new Random().nextInt(lst.size()));
+            int repeat = 0;
+            while (repeat != 8) {
+                String user = input.nextLine();
+                if (word.equals(user)) {
+                    System.out.println("You survived!");
+                    break;
+                } else {
+                    System.out.println("You lost!");
+                    repeat++;
+                }
+            }
         }
     }
-}
