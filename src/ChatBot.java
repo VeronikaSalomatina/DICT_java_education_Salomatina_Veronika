eeec7d7cc691f9ab390845048f45d938e84e2bf8
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 21:04:54 +0200

    ChatBot {3-rd} stage

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
-
 public class ChatBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Veronika.");
-        System.out.println("I was created in 2021.");
-        System.out.println("Please, remind me your name.");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! My name is Veronika.\nI was created in 2021.\nPlease, remind me your name.");
         String your_name = scanner.nextLine();
         System.out.println("What a great name you have, " + your_name + "!");
+        System.out.println("Let me guess your age.\nНужно вписать остатки от деления своего возраста на 3,5 и 7");
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
+
