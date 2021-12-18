1fdbb5a8c215810ab5080a7cf9b4f10f35b8f373
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 22:04:36 +0200

    Hangman {5-th} stage

--- src/Hangman.java
+++ src/Hangman.java
@@ -5,6 +5,7 @@ public class Hangman {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         StringBuffer buffer = new StringBuffer("");
+        StringBuffer user_word = new StringBuffer("");
         System.out.println("HANGMAN");
         ArrayList<String> lst = new ArrayList<>();
         lst.add("python");
@@ -19,14 +20,33 @@ public class Hangman {
         for (int i = 1; i <= word.length() - 2; i++) help.append("-");
         while (repeat != 8) {
             System.out.println("Guess the word" + " " + help + ":");
-            String user = input.nextLine();
-            if (word.equals(user)) {
-                System.out.println("You survived!");
-                break;
-            } else {
-                System.out.println("You lost!");
-                repeat++;
+            int heart = 8;
+            for (int i = 1; i <= word.length(); i++) user_word.append("-");
+            while (heart != 0) {
+                System.out.println(user_word);
+                System.out.println("Input a letter:");
+                String user = input.nextLine();
+                if (word.equals(user)) {
+                    System.out.println("You survived!");
+                    break;
+                    int index = word.indexOf(user);
+                    if (index >= 0){
+                        if (user.equals("a")){
+                            user_word.insert(index, user);
+                            user_word.delete(index + 1, index + 2);
+                            user_word.insert(index + 2, user);
+                            user_word.delete(index + 3, index + 4);
+                        } else {
+                            user_word.insert(index, user);
+                            user_word.delete(index + 1, index + 2);
+                        }
+                    } else {
+                        System.out.println("You lost!");
+                        repeat++;
+                        System.out.println("Than letter doesn't appear in the word");
+                    }
+                    heart--;
+                }
+                System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
             }
         }
-    }
-}
