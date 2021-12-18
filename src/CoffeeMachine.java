4364f33100958cbedf032064c4aa289604077706
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 23:37:50 +0200

    CoffeeMachine {4-th} stage

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,3 +1,4 @@
+package CoffeeMachine;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,16 +16,77 @@ public class CoffeeMachine {
 //        System.out.println(min.getAsInt());
         System.out.println("Write how many cups of coffee you will need:");
         int cup = input.nextInt();
-        System.out.println("For " + cup + " cups of coffee you will need:\n" +
-                cup * 200 + " ml of water\n" +
-                cup * 50 + " ml of milk\n" +
-                cup * 15 + " g of coffee beans");
         if (min.getAsInt() == cup){
             System.out.println("Yes, I can make that amount of coffee");
         } else if (min.getAsInt() > cup) {
             System.out.println("Yes, I can make that amount of coffee (and even " + (min.getAsInt() - cup) + " more than that)");
         }else {
             System.out.println("No, I can make only " + min.getAsInt() + " cups of coffee");
+
+            // w = water, m = milk, cb = coffee beans, c = cup, $ = money
+            Integer[] w_m_cb_c_$ = new Integer[]{400, 540, 120, 9, 550};
+
+            System.out.println(Output.output(w_m_cb_c_$));
+            System.out.println("\nWrite action (buy, fill, take):");
+            String action = input.nextLine();
+            switch (action) {
+                case "buy" -> {
+                    System.out.println("What do you want? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                    int drink = input.nextInt();
+                    switch (drink) {
+                        case 1 -> {  // espresso
+                            w_m_cb_c_$[0] -= 250;
+                            w_m_cb_c_$[2] -= 16;
+                            w_m_cb_c_$[4] += 4;
+                        }
+                        case 2 -> {  // latte
+                            w_m_cb_c_$[0] -= 350;
+                            w_m_cb_c_$[1] -= 75;
+                            w_m_cb_c_$[2] -= 20;
+                            w_m_cb_c_$[4] += 7;
+                        }
+                        case 3 -> {  // cappuccino
+                            w_m_cb_c_$[0] -= 200;
+                            w_m_cb_c_$[1] -= 100;
+                            w_m_cb_c_$[2] -= 12;
+                            w_m_cb_c_$[4] += 6;
+                        }
+                    }
+                    w_m_cb_c_$[3]--;
+                    System.out.println(Output.output(w_m_cb_c_$));
+                }
+                case "fill" -> {
+                    System.out.println("Write how many ml of water you want to add:");
+                    int water_to_add = input.nextInt();
+                    System.out.println("Write how many ml of milk you want to add:");
+                    int milk_to_add = input.nextInt();
+                    System.out.println("Write how many grams of coffee beans you want to add:");
+                    int cb_to_add = input.nextInt();
+                    System.out.println("Write how many disposable coffee cups you want to add:");
+                    int cups_to_add = input.nextInt();
+                    w_m_cb_c_$[0] += water_to_add;
+                    w_m_cb_c_$[1] += milk_to_add;
+                    w_m_cb_c_$[2] += cb_to_add;
+                    w_m_cb_c_$[3] += cups_to_add;
+                    System.out.println(Output.output(w_m_cb_c_$));
+                }
+                case "take" -> {
+                    System.out.println("I give you " + w_m_cb_c_$[4]);
+                    w_m_cb_c_$[4] -= w_m_cb_c_$[4];
+                    System.out.println(Output.output(w_m_cb_c_$));
+                }
+            }
         }
     }
-}
+
+    class Output {
+        // w = water, m = milk, cb = coffee beans, c = cup, $ = money
+        public static String output(Integer[] w_m_cb_c_$){
+            return ("\nThe coffee machine has:\n" +
+                    w_m_cb_c_$[0] + " of water\n" +
+                    w_m_cb_c_$[1] + " of milk\n" +
+                    w_m_cb_c_$[2] + " of coffee beans\n" +
+                    w_m_cb_c_$[3] + " of disposable cups\n" +
+                    w_m_cb_c_$[4] + " of money");
+        }
+    }
