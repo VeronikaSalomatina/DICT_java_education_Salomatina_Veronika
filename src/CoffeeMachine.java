83b56e28dd98a056c4e92c6bdefb394bc5d343ef
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-19 00:11:11 +0200

    CoffeeMachine {5-th} stage

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,92 +1,118 @@
-package CoffeeMachine;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = input.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = input.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee_beans = input.nextInt();
-        var count = new int[] {water / 200, milk / 50, coffee_beans / 15};
-        var min = Arrays.stream(count).min();
-//        System.out.println(min.getAsInt());
-        System.out.println("Write how many cups of coffee you will need:");
-        int cup = input.nextInt();
-        if (min.getAsInt() == cup){
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (min.getAsInt() > cup) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (min.getAsInt() - cup) + " more than that)");
-        }else {
-            System.out.println("No, I can make only " + min.getAsInt() + " cups of coffee");
+        Integer[] resources = new Integer[]{400, 540, 120, 9, 550};
 
-            // w = water, m = milk, cb = coffee beans, c = cup, $ = money
-            Integer[] w_m_cb_c_$ = new Integer[]{400, 540, 120, 9, 550};
-
-            System.out.println(Output.output(w_m_cb_c_$));
-            System.out.println("\nWrite action (buy, fill, take):");
-            String action = input.nextLine();
+        while (true) {
+            System.out.println("\nWrite action (buy, fill, take, remaining, exit):");
+            String action = (String) Input("String");
+            if (action.equals("exit")) {
+                break;
+            }
             switch (action) {
                 case "buy" -> {
-                    System.out.println("What do you want? 1 - espresso, 2 - latte, 3 - cappuccino:");
-                    int drink = input.nextInt();
-                    switch (drink) {
-                        case 1 -> {  // espresso
-                            w_m_cb_c_$[0] -= 250;
-                            w_m_cb_c_$[2] -= 16;
-                            w_m_cb_c_$[4] += 4;
-                        }
-                        case 2 -> {  // latte
-                            w_m_cb_c_$[0] -= 350;
-                            w_m_cb_c_$[1] -= 75;
-                            w_m_cb_c_$[2] -= 20;
-                            w_m_cb_c_$[4] += 7;
-                        }
-                        case 3 -> {  // cappuccino
-                            w_m_cb_c_$[0] -= 200;
-                            w_m_cb_c_$[1] -= 100;
-                            w_m_cb_c_$[2] -= 12;
-                            w_m_cb_c_$[4] += 6;
-                        }
-                    }
-                    w_m_cb_c_$[3]--;
-                    System.out.println(Output.output(w_m_cb_c_$));
+                    Buy(resources);
                 }
                 case "fill" -> {
-                    System.out.println("Write how many ml of water you want to add:");
-                    int water_to_add = input.nextInt();
-                    System.out.println("Write how many ml of milk you want to add:");
-                    int milk_to_add = input.nextInt();
-                    System.out.println("Write how many grams of coffee beans you want to add:");
-                    int cb_to_add = input.nextInt();
-                    System.out.println("Write how many disposable coffee cups you want to add:");
-                    int cups_to_add = input.nextInt();
-                    w_m_cb_c_$[0] += water_to_add;
-                    w_m_cb_c_$[1] += milk_to_add;
-                    w_m_cb_c_$[2] += cb_to_add;
-                    w_m_cb_c_$[3] += cups_to_add;
-                    System.out.println(Output.output(w_m_cb_c_$));
+                    Fill(resources);
                 }
                 case "take" -> {
-                    System.out.println("I give you " + w_m_cb_c_$[4]);
-                    w_m_cb_c_$[4] -= w_m_cb_c_$[4];
-                    System.out.println(Output.output(w_m_cb_c_$));
+                    System.out.println("I give you " + resources[4]);
+                    resources[4] -= resources[4];
+                }
+                case "remaining" -> {
+                    System.out.println(Remaining(resources));
                 }
             }
         }
     }
+    public static void Buy(Integer[] resources) {
+        System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String drink = (String) Input("String");
 
-    class Output {
-        // w = water, m = milk, cb = coffee beans, c = cup, $ = money
-        public static String output(Integer[] w_m_cb_c_$){
-            return ("\nThe coffee machine has:\n" +
-                    w_m_cb_c_$[0] + " of water\n" +
-                    w_m_cb_c_$[1] + " of milk\n" +
-                    w_m_cb_c_$[2] + " of coffee beans\n" +
-                    w_m_cb_c_$[3] + " of disposable cups\n" +
-                    w_m_cb_c_$[4] + " of money");
+        switch (drink) {
+            case "1" -> {  // espresso
+                if (resources[0] < 250 || resources[2] < 16 || resources[3] < 0){
+                    Integer[] lst_for_check = new Integer[] {resources[0] - 250, 0, resources[2] - 16, resources[3] - 1};
+                    System.out.println(Check(lst_for_check));
+                } else {
+                    Integer[] minus = new Integer[]{250, 0, 16, 0, 4};
+                    Minus_resources(resources, minus);
+                }
+            }
+            case "2" -> {  // latte
+                if (resources[0] < 350 || resources[1] < 75 || resources[2] < 20 || resources[3] < 0) {
+                    Integer[] lst_for_check = new Integer[]{resources[0] - 350, resources[1] - 75, resources[2] - 20, resources[3] - 1};
+                    System.out.println(Check(lst_for_check));
+                } else {
+                    Integer[] minus = new Integer[]{350, 75, 20, 0, 7};
+                    Minus_resources(resources, minus);
+                }
+            }
+            case "3" -> {  // cappuccino
+                if (resources[0] < 200 || resources[1] < 100 || resources[2] < 12 || resources[3] < 0) {
+                    Integer[] lst_for_check = new Integer[]{resources[0] - 200, resources[1] - 100, resources[2] - 12, resources[3] - 1};
+                    System.out.println(Check(lst_for_check));
+                } else {
+                    Integer[] minus = new Integer[]{200, 100, 12, 0, 6};
+                    Minus_resources(resources, minus);
+                }
+            }
+            case "back" -> {
+            }
         }
     }
+
+    public static void Fill(Integer[] resources){
+        System.out.println("\nWrite how many ml of water you want to add:");
+        int water_to_add = (int) Input("Integer");
+        System.out.println("Write how many ml of milk you want to add:");
+        int milk_to_add = (int) Input("Integer");
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        int cb_to_add = (int) Input("Integer");
+        System.out.println("Write how many disposable coffee cups you want to add:");
+        int cups_to_add = (int) Input("Integer");
+        resources[0] += water_to_add;
+        resources[1] += milk_to_add;
+        resources[2] += cb_to_add;
+        resources[3] += cups_to_add;
+    }
+
+    public static String Remaining(Integer[] resources){
+        return ("\nThe coffee machine has:\n" +
+                resources[0] + " of water\n" +
+                resources[1] + " of milk\n" +
+                resources[2] + " of coffee beans\n" +
+                resources[3] + " of disposable cups\n" +
+                resources[4] + " of money");
+    }
+
+    public static String Check(Integer[] lst_for_check){
+        String not_enough;
+        if (lst_for_check[0] < 0) {
+            not_enough = "water";
+        } else if (lst_for_check[1] < 0) {
+            not_enough = "milk";
+        } else if (lst_for_check[2] < 0){
+            not_enough = "coffee beans";
+        } else {not_enough = "cups";}
+        return ("Sorry, not enough " + not_enough + "!");
+    }
+    static Object Input(String condition) {
+        Scanner input = new Scanner(System.in);
+        if ("String".equals(condition)) {
+            return input.nextLine();
+        } else {return input.nextInt();}
+    }
+
+    public static void Minus_resources(Integer[] resources, Integer[] minus) {
+        resources[0] -= minus[0];
+        resources[1] -= minus[1];
+        resources[2] -= minus[2];
+        resources[3]--;
+        resources[4] += minus[4];
+
+        System.out.println("I have enough resources, making you a coffee!");
+    }
+}
