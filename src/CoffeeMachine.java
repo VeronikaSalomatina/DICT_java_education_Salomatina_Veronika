3c6b38fbba4a9e0dbbcd1fcb33999e12482ba9ad
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 23:33:59 +0200

    CoffeeMachine {2-nd} stage

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int cup = input.nextInt();
+        System.out.println("For " + cup + " cups of coffee you will need:\n" +
+                cup * 200 + " ml of water\n" +
+                cup * 50 + " ml of milk\n" +
+                cup * 15 + " g of coffee beans");
+    }
 }
