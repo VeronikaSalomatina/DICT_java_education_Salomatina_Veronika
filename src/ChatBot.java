1f2e030695eb27538fc42c02cf7bb9f33eb5a65a
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 20:54:44 +0200

    ChatBot {2-nd} stage

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Veronica.");
+        System.out.println("Hello! My name is Veronika.");
         System.out.println("I was created in 2021.");
+        System.out.println("Please, remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String your_name = scanner.nextLine();
+        System.out.println("What a great name you have, " + your_name + "!");
     }
 }
