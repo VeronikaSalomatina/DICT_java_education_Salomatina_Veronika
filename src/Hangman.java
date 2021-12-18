7f7510e6a8cbcbd8a4eae65147d4c3e8d0069c48
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 22:13:59 +0200

    Hangman {6-th} stage

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,49 +4,56 @@ import java.util.Random;
 public class Hangman {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        StringBuffer buffer = new StringBuffer("");
         StringBuffer user_word = new StringBuffer("");
         System.out.println("HANGMAN");
         ArrayList<String> lst = new ArrayList<>();
+        ArrayList<String> storage = new ArrayList<>();
         lst.add("python");
         lst.add("java");
         lst.add("javascript");
         lst.add("kotlin");
-
         String word = lst.get(new Random().nextInt(lst.size()));
-        int repeat = 0;
-        buffer.append(word);
-        StringBuffer help = buffer.delete(2, buffer.length());
-        for (int i = 1; i <= word.length() - 2; i++) help.append("-");
-        while (repeat != 8) {
-            System.out.println("Guess the word" + " " + help + ":");
-            int heart = 8;
-            for (int i = 1; i <= word.length(); i++) user_word.append("-");
-            while (heart != 0) {
-                System.out.println(user_word);
-                System.out.println("Input a letter:");
-                String user = input.nextLine();
-                if (word.equals(user)) {
-                    System.out.println("You survived!");
-                    break;
-                    int index = word.indexOf(user);
-                    if (index >= 0){
-                        if (user.equals("a")){
-                            user_word.insert(index, user);
-                            user_word.delete(index + 1, index + 2);
-                            user_word.insert(index + 2, user);
-                            user_word.delete(index + 3, index + 4);
-                        } else {
-                            user_word.insert(index, user);
-                            user_word.delete(index + 1, index + 2);
-                        }
+        int heart = 8;
+        for (int i = 1; i <= word.length(); i++) user_word.append("-");
+        while (heart != 0) {
+            System.out.println(user_word);
+            System.out.println("\n" + user_word);
+            System.out.println("Input a letter:");
+            String user = input.nextLine();
+            int index = word.indexOf(user);
+            if (index >= 0){
+                if (index < 0 || storage.contains(user)){
+                    if (storage.contains(user)) {
+                        System.out.println("No improvements");
                     } else {
-                        System.out.println("You lost!");
-                        repeat++;
                         System.out.println("Than letter doesn't appear in the word");
                     }
+                    storage.add(user);
                     heart--;
+
+                } else {
+                    storage.add(user);
+                    if (user.equals("a")){
+                        user_word.insert(index, user);
+                        user_word.delete(index + 1, index + 2);
+                        user_word.insert(index + 2, user);
+                        user_word.delete(index + 3, index + 4);
+                    } else {
+                        user_word.insert(index, user);
+                        user_word.delete(index + 1, index + 2);
+                    }
+                } else {
+                    System.out.println("Than letter doesn't appear in the word");
+                    if (word.contains(user_word)) {
+                        System.out.println(user_word + "\nYou guessed the word!\nYou survived!");
+                        break;
+                    }
                 }
-                System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
+                heart--;
+            }
+            System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
+            if (0 >= heart){
+                System.out.println("You lost!");
             }
         }
+    }
