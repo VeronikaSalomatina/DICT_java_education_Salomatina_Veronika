6896fa8f72c584cc03cdaeb3db9207ccfe12261d
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 20:45:09 +0200

    ChatBot {1-st} stage

---
+++ src/ChatBot.java
@@ -0,0 +1,6 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Veronica.");
+        System.out.println("I was created in 2021.");
+    }
+}
