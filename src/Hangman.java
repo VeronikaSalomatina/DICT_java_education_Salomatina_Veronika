7520773ae45ff42284b84d7628af4c89daaace69
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-18 21:45:38 +0200

    Hangman {2-nd} stage

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,17 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+
 public class Hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN\nThe game will be available soon.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("HANGMAN");
+
+        String user = scanner.nextLine();
+        if (Objects.equals(user, "java")){
+            System.out.println("You survived!");
+        } else{
+            System.out.println("You lost!");
+        }
     }
 }
