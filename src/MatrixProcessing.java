f2bfb4df688b35db730ae1cf546cccf82df0fb20
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-26 22:26:55 +0200

    MatrixProcessing {4-th} stage

--- src/MatrixProcessing.java
+++ src/MatrixProcessing.java
@@ -2,124 +2,162 @@ import java.util.Scanner;
 
 public class MatrixProcessing {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        while (true){
-            System.out.println("""
-            1. Add matrices
-            2. Multiply matrix by constant
-            3. Multiply matrices
-            4. Exit
-            """);
-            System.out.println("Choose your action:");
-            String user = scan.next();
-            if (user.equals("1")){
-                sum();
+        Scanner in = new Scanner(System.in);
+        int c;
+        int a;
+        int[] size = new int[2];
+        int[] size2 = new int[2];
+        int[][] matrix = new int[0][];
+        int[][] matrix2 = new int[0][];
+        int[][] matrix3 = new int[0][];
+        for (; ; ) {
+            System.out.println("1. Add matrices\n" +
+                    "2. Multiply matrix by a constant\n" +
+                    "3. Multiply matrices\n" +
+                    "4. Transpose matrix\n" +
+                    "5. Exit");
+            System.out.print("You choice:");
+            a = in.nextInt();
+            if (a == 5){
+                System.exit(0);
             }
-            else if (user.equals("2")){
-                constants();
-            }
-            else if (user.equals("3")){
-                mult();
-            }
-            else {
-                break;
-            }
-        }
-    }
-    public static void sum(){
-        Scanner scanner = new Scanner(System.in);
-        int row_first = scanner.nextInt();
-        int column_first = scanner.nextInt();
-        int[][] first_matrix = new int[row_first][column_first];
-        for (int i = 0; i < row_first; i++) {
-            for (int j = 0; j < column_first; j++) {
-                first_matrix[i][j] = scanner.nextInt();
-            }
-        }
-        int row_second = scanner.nextInt();
-        int column_second = scanner.nextInt();
-        int[][] second_matrix = new int[row_second][column_second];
-        for (int i = 0; i < row_second; i++) {
-            for (int j = 0; j < column_second; j++) {
-                second_matrix[i][j] = scanner.nextInt();
-            }
-        }
-        if (first_matrix.length == second_matrix.length) {
-            System.out.println("Sum: ");
-            for (int i = 0; i < row_first; i++) {
-                for (int j = 0; j < column_second; j++) {
-                    System.out.print(first_matrix[i][j] + second_matrix[i][j] + " ");
+            System.out.print("Enter size matrix: ");
+            size[0] = in.nextInt();
+            size[1] = in.nextInt();
+            matrix = new int[size[0]][size[1]];
+            System.out.println("Enter matrix: ");
+            for (int q = 0; q < size[0]; q++) {
+                for (int w = 0; w < size[1]; w++) {
+                    matrix[q][w] = in.nextInt();
                 }
-                System.out.println();
-            }
-        }
-        else {System.out.println("ERROR");
-        }
-    }
-    public static void constants(){
-        Scanner scanner_2 = new Scanner(System.in);
-        int line_A = scanner_2.nextInt();
-        int column_A = scanner_2.nextInt();
-        int[][] matrixA = new int[line_A][column_A];
-        for (int i = 0; i < line_A; i++) {
-            for (int j = 0; j < column_A; j++) {
-                matrixA[i][j] = scanner_2.nextInt();
-            }
-        }
-        int constant = scanner_2.nextInt();
-        System.out.println("Multiplication Constanta: ");
-        for (int i = 0; i < line_A; i++) {
-            for (int j = 0; j < column_A; j++) {
-                System.out.print(matrixA[i][j] * constant+ " ");
-            }
-            System.out.println();
-        }
-    }
-    public static void mult(){
-        Scanner scanner_3 = new Scanner(System.in);
-        System.out.print("Enter size of first matrix: ");
-        int[] first_matrix = new int[2];
-        for (int i = 0; i < 2; i++) {
-            first_matrix[i] = scanner_3.nextInt();
-        }
-        System.out.println("Enter first  matrix: ");
-        double[][] first_matrix_value = new double[first_matrix[0]][first_matrix[1]];
-        for (int i = 0; i < first_matrix[0]; i++) {
-            for (int j = 0; j < first_matrix[1]; j++) {
-                first_matrix_value[i][j] = scanner_3.nextDouble();
-            }
-        }
-        System.out.print("Enter second matrix: ");
-        int[] second_matrix = new int[2];
-        for (int i = 0; i < 2; i++) {
-            second_matrix[i] = scanner_3.nextInt();
-        }
-        System.out.println("Enter second matrix: ");
-        double[][] second_matrix_value = new double[second_matrix[0]][second_matrix[1]];
-        for (int i = 0; i < second_matrix[0]; i++) {
-            for (int j = 0; j < second_matrix[1]; j++) {
-                second_matrix_value [i][j] = scanner_3.nextDouble();
-            }
-        }
-        double[][] resultMultiplication = new double[first_matrix[0]][second_matrix[1]];
-        for (int line_a = 0; line_a < first_matrix[0]; line_a++) {
-            for (int column_b = 0; column_b < second_matrix[1]; column_b++) {
-                resultMultiplication [line_a][column_b] = 0;
-            }
-        }
-        if (second_matrix[0] == first_matrix[1]) {
-            System.out.println("Multiplication");
-            for (int line = 0; line < first_matrix[0]; line++) {
-                for (int column = 0; column < second_matrix[1]; column++) {
-                    for (int value = 0; value < first_matrix[1]; value++) {
-                        resultMultiplication[line][column] = resultMultiplication[line][column] + first_matrix_value[line][value] * second_matrix_value[value][column];
+                matrix2 = new int[size[0]][size[1]];
+                if (a == 1) {
+                    System.out.println("Enter second matrix: ");
+                    matrix3 = new int[size[0]][size[1]];
+                    for (q = 0; q < size[0]; q++) {
+                        for (int w = 0; w < size[1]; w++) {
+                            matrix2[q][w] = in.nextInt();
+                        }
                     }
-                    System.out.print(resultMultiplication[line][column] + " ");
+                    for (q = 0; q < size[0]; q++) {
+                        for (int w = 0; w < size[1]; w++) {
+                            matrix3[q][w] = matrix[q][w] + matrix2[q][w];
+                        }
+                    }
+                    for (q = 0; q < size[0]; q++) {
+                        for (int w = 0; w < size[1]; w++) {
+                            System.out.print("|" + matrix3[q][w] + "|");
+                        }
+                        System.out.println();
+                    }
+                }else if (a==2){
+                    System.out.println("Enter constant: ");
+                    c = in.nextInt();
+                    for (q = 0; q < size[0]; q++) {
+                        for (int w = 0; w < size[1]; w++) {
+                            matrix2[q][w] = matrix[q][w]*c;
+                        }
+                    }
+                    for (q = 0; q < size[0]; q++) {
+                        for (int w = 0; w < size[1]; w++) {
+                            System.out.print("|" + matrix2[q][w] + "|");
+                        }
+                        System.out.println();
+                    }
+                }else if (a==3){
+                    System.out.print("Enter size second matrix: ");
+                    size2[0] = in.nextInt();
+                    size2[1] = in.nextInt();
+                    matrix2 = new int [size[0]][size[1]];
+                    System.out.println("Enter second matrix: ");
+                    for (int i = 0; i < size2[0]; i++) {
+                        for (int i1 = 0; i1 < size2[1]; i1++) {
+                            matrix2[i][i1] = in.nextInt();
+                        }
+                    }
+                    matrix3 = new int[size[0]][size2[1]];
+                    for (q = 0; q < size[0]; q++) {
+                        for (int w = 0; w < size2[1]; w++) {
+                            matrix3[q][w] = 0;
+                        }
+                    }
+                    if (size[1] == size2[0]) {
+                        for (q = 0; q < size[0]; q++) {
+                            for (int w = 0; w < size2[1]; w++) {
+                                for (int e = 0; e < size[1]; e++) {
+                                    matrix3[q][w] = matrix3[q][w] + matrix[q][e]* matrix2[e][w];
+                                }
+                            }
+                        }
+                        for (q = 0; q < size[0]; q++) {
+                            for (int w = 0; w < size[1]; w++) {
+                                System.out.print("|" + matrix3[q][w] + "|");
+                            }
+                            System.out.println();
+                        }
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                } else if (a == 4) {
+                    System.out.println("1. Main diagonal\n2. Side diagonal\n3. Vertical line\n4. Horizontal line");
+                    a = in.nextInt();
+                    matrix2 = new int[size[1]][size[0]];
+                    matrix3 = new int[size[0]][size[1]];
+                    if (a == 1) {
+                        for (q = 0; q < size[1]; ++q) {
+                            for (int w = 0; w < size[0]; ++w) {
+                                matrix2[q][w] = matrix[w][q];
+                            }
+                        }
+                        for (q = 0; q < size[1]; ++q) {
+                            for (int w = 0; w < size[0]; ++w) {
+                                System.out.print("|" + matrix2[q][w] + "|");
+                            }
+                            System.out.println();
+                        }
+                    } else if (a == 2) {
+                        for (q = 0; q < size[0]; ++q) {
+                            for (int w = 0; w < size[1]; ++w) {
+                                matrix2[w][q] = matrix[size[0] - (q + 1)][size[1] - (w + 1)];
+                            }
+                        }
+                        for (q = 0; q < size[1]; ++q) {
+                            for (int w = 0; w < size[0]; ++w) {
+                                System.out.print("|" + matrix2[q][w] + "|");
+                            }
+                        }
+                        System.out.println();
+                    }
+                } else if (a == 3) {
+                    for (q = 0; q < size[0]; ++q) {
+                        for (int w = 0; w < size[1]; ++w) {
+                            matrix3[q][w] = matrix[q][size[1] - (w + 1)];
+                        }
+                        System.out.println();
+                    }
+                    for (q = 0; q < size[0]; ++q) {
+                        for (int w = 0; w < size[1]; ++w) {
+                            System.out.print("|" + matrix3[q][w] + "|");
+                        }
+                        System.out.println();
+                    }
+                } else if (a == 4) {
+                    for (q = 0; q < size[0]; ++q) {
+                        for (int w = 0; w < size[1]; ++w) {
+                            matrix3[q][w] = matrix[size[0] - (q + 1)][w];
+                        }
+                        System.out.println();
+                    }
+                    for (q = 0; q < size[0]; ++q) {
+                        for (int w = 0; w < size[1]; ++w) {
+                            System.out.print("|" + matrix3[q][w] + "|");
+                        }
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("ERROR");
                 }
-                System.out.println();
             }
-        } else {
-            System.out.println("ERROR");
         }
     }
 }
