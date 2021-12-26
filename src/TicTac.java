8aad0c616c90a7e173408fbde66cb3292cb387ad
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-26 21:06:30 +0200

    TicTac {3-rd} stage

--- src/TicTac.java
+++ src/TicTac.java
@@ -1,20 +1,71 @@
 import java.util.Scanner;
 
-
 public class TicTacToe {
+
+    Scanner userInput = new Scanner(System.in);
+    String field = "";
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String game_net = input.nextLine();
-        char[] row1 = new char[3];
-        char[] row2 = new char[3];
-        char[] row3 = new char[3];
-        game_net.getChars(0, 3, row1, 0);
-        game_net.getChars(3, 6, row2, 0);
-        game_net.getChars(6, 9, row3, 0);
-        System.out.println("---------");
-        System.out.println("| " + row1[0] + " " + row1[1] + " " + row1[2] + " |");
-        System.out.println("| " + row2[0] + " " + row2[1] + " " + row2[2] + " |");
-        System.out.println("| " + row3[0] + " " + row3[1] + " " + row3[2] + " |");
-        System.out.println("---------");
+        TicTacToe myGame = new TicTacToe();
+        myGame.setField();
+        myGame.showField();
+        System.out.println(myGame.checkGameStatus());
+    }
+
+    public int countMatches(String target) {
+        String deleteTarget = field.replace (target, "");
+        return field.length () - deleteTarget.length ();
+    }
+    public void setField
+            () {
+        System.out.println("Enter cells:");
+        String newField = userInput.nextLine();
+        field = newField;
+    }
+
+    public void showField() {
+        System.out.printf("---------\n" +
+                "| %s |\n" +
+                "| %s |\n" +
+                "| %s |\n" +
+                "---------", field.substring(0, 3), field.substring(3, 6), field.substring(6, 9));
+    }
+    public String checkGameStatus() {
+        boolean oWin = false;
+        boolean xWin = false;
+
+        if (field.charAt(0) == 'O' && field.charAt(1) == 'O' && field.charAt(2) == 'O' || field.charAt(3) == 'O' &&
+                field.charAt(4) == 'O' && field.charAt(5) == 'O' || field.charAt(6) == 'O' && field.charAt(7) == 'O' &&
+                field.charAt(8) == 'O' || field.charAt(0) == 'O' && field.charAt(3) == 'O' && field.charAt(6) == 'O' ||
+                field.charAt(1) == 'O' && field.charAt(4) == 'O' && field.charAt(7) == 'O' || field.charAt(2) == 'O' &&
+                field.charAt(5) == 'O' && field.charAt(8) == 'O' || field.charAt(0) == 'O' && field.charAt(4) == 'O' &&
+                field.charAt(8) == 'O' || field.charAt(2) == 'O' && field.charAt(4) == 'O' && field.charAt(6) == 'O') {
+            oWin = true;
+        }
+
+        if (field.charAt(0) == 'X' && field.charAt(1) == 'X' && field.charAt(2) == 'X' || field.charAt(3) == 'X' &&
+                field.charAt(4) == 'X' && field.charAt(5) == 'X' || field.charAt(6) == 'X' && field.charAt(7) == 'X' &&
+                field.charAt(8) == 'X' || field.charAt(0) == 'X' && field.charAt(3) == 'X' && field.charAt(6) == 'X' ||
+                field.charAt(1) == 'X' && field.charAt(4) == 'X' && field.charAt(7) == 'X' || field.charAt(2) == 'X' &&
+                field.charAt(5) == 'X' && field.charAt(8) == 'X' || field.charAt(0) == 'X' && field.charAt(4) == 'X' &&
+                field.charAt(8) == 'X' || field.charAt(2) == 'X' && field.charAt(4) == 'X' && field.charAt(6) == 'X') {
+            xWin = true;
+        }
+
+        if (countMatches("X") - countMatches("O") > 1 || countMatches("O") -
+                countMatches("X")> 1) {
+            return "\nImpossible";
+        } else if (xWin && oWin) {
+            return "\nImpossible";
+        } else if (xWin) {
+            return "\nX is winner!";
+        } else if (oWin) {
+            return "\nO is winner!";
+        } else if (field.contains("_")) {
+            return "\nGame unfinished";
+        } else {
+            return "\nDraw";
+        }
+
     }
 }
