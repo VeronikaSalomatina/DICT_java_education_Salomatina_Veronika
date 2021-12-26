74cbcd889bdebbaf8bfd149af2dac080b68c904d
Salomatina Veronika <v.a.salomatina@student.khai.edu>
2021-12-26 21:03:43 +0200

    TicTac {2-nd} stage

--- src/TicTac.java
+++ src/TicTac.java
@@ -1,7 +1,20 @@
-public class TicTac {
+import java.util.Scanner;
+
+
+public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("""
-                X O X\nO X O\nX X O
-                """);
+        Scanner input = new Scanner(System.in);
+        String game_net = input.nextLine();
+        char[] row1 = new char[3];
+        char[] row2 = new char[3];
+        char[] row3 = new char[3];
+        game_net.getChars(0, 3, row1, 0);
+        game_net.getChars(3, 6, row2, 0);
+        game_net.getChars(6, 9, row3, 0);
+        System.out.println("---------");
+        System.out.println("| " + row1[0] + " " + row1[1] + " " + row1[2] + " |");
+        System.out.println("| " + row2[0] + " " + row2[1] + " " + row2[2] + " |");
+        System.out.println("| " + row3[0] + " " + row3[1] + " " + row3[2] + " |");
+        System.out.println("---------");
     }
 }
